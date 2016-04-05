4aa3931d30fac4d5ee5bf81a957f00f7e301670d
itgsod-oscar-larsson <oscar.larsson@elev.it-gymnasiet.se>
2016-04-05 11:14:40 +0200

    Initial commit

---
+++ godisautomat.py
@@ -0,0 +1,44 @@
+print "1, chocklad 20kr"
+
+print "2, festis 10kr"
+
+print "3, Pepsi 14kr"
+
+print "4, chips 17kr"
+
+print "5, klubba 5kr"
+
+print "6, daim 20kr"
+
+print "7, cola 13kr"
+
+print "8, nappar 14kr"
+
+print "9, fanta 18kr"
+
+
+nr = raw_input("Vad vill du ha? ")
+
+if nr == "1":
+    print "Ge mig 20 kr"
+elif nr =="2":
+    print "Ge mig 10 kr"
+elif nr =="3":
+    print "Ge mig 14 kr"
+elif nr =="4":
+    print "Ge mig 17 kr"
+elif nr =="5":
+    print "Ge mig 5 kr"
+elif nr =="6":
+    print "Ge mig 20 kr"
+elif nr =="7":
+    print "Ge mig 13 kr"
+elif nr =="8":
+    print "Ge mig 14 kr"
+elif nr =="9":
+    print "Ge mig 18 kr"
+else:
+    print "Ok Tack och hejda"
+
+
+
